672e52b5403b8085db06375d321ebe01284e8cec
Shresta0522 <shrestareddy82@gmail.com>
2025-04-13 16:34:12 -0400

    Selenium Assignment

---
+++ Selenium_Assignment/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Selenium_Assignment
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
